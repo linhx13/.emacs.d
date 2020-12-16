6f550fd53cf741766180010299409463787cd4e0
linhx13 <mylhx288@gmail.com>
2020-12-16 19:30:03 +0800

    Fix org latext preview divsvgm.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -256,31 +256,31 @@ prepended to the element after the #+HEADER: tag."
            ("P" . org-pomodoro)))
 
   ;; LaTeX
-  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.0))
   (setq org-latex-packages-alist
         '(("fontset=macnew,UTF8" "ctex" t)))
-  (setq org-preview-latex-default-process 'imagemagick)
-  (setq org-preview-latex-process-alist
-        '(
-          (dvisvgm
-           :programs ("xelatex" "dvisvgm")
-           :description "xdv > svg"
-           :message "you need to install the programs: xelatex and dvisvgm."
-           :image-input-type "xdv"
-           :image-output-type "svg"
-           :image-size-adjust (1.7 . 1.5)
-           :latex-compiler ("xelatex --no-pdf -interaction nonstopmode -output-directory %o %f")
-           :image-converter ("dvisvgm %f -n -b min -c %S -o %O"))
-          (imagemagick
-           :programs ("latex" "convert")
-           :description "pdf > png"
-           :message "you need to install the programs: xelatex and imagemagick."
-           :image-input-type "pdf"
-           :image-output-type "png"
-           :image-size-adjust (1.0 . 1.0)
-           :latex-compiler ("xelatex -interaction nonstopmode -output-directory %o %f")
-           :image-converter
-           ("convert -density %D -trim -antialias %f -quality 100 %O"))))
+  (setq org-preview-latex-default-process 'dvisvgm)
+  ;; (setq org-preview-latex-process-alist
+  ;;       '(
+  ;;         (dvisvgm
+  ;;          :programs ("xelatex" "dvisvgm")
+  ;;          :description "xdv > svg"
+  ;;          :message "you need to install the programs: xelatex and dvisvgm."
+  ;;          :image-input-type "xdv"
+  ;;          :image-output-type "svg"
+  ;;          :image-size-adjust (1.7 . 1.5)
+  ;;          :latex-compiler ("xelatex --no-pdf -interaction nonstopmode -output-directory %o %f")
+  ;;          :image-converter ("dvisvgm %f -n -b min -c %S -o %O"))
+  ;;         (imagemagick
+  ;;          :programs ("latex" "convert")
+  ;;          :description "pdf > png"
+  ;;          :message "you need to install the programs: xelatex and imagemagick."
+  ;;          :image-input-type "pdf"
+  ;;          :image-output-type "png"
+  ;;          :image-size-adjust (1.0 . 1.0)
+  ;;          :latex-compiler ("xelatex -interaction nonstopmode -output-directory %o %f")
+  ;;          :image-converter
+  ;;          ("convert -density %D -trim -antialias %f -quality 100 %O"))))
   )
 
 ;; org-roam
